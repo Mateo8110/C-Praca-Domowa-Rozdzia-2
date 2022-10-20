56f32d027498f8b9e31736e96955d558ccb68f87
Mateo8110 <94134843+Mateo8110@users.noreply.github.com>
2022-10-20 13:29:14 +0200

    Add files via upload

---
+++ Zadanie 2.9f.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Zadanie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1, y = 3, z = 4;
+            bool wynik = (x == 1 | y++ > 2 | ++z > 0);
+            Console.WriteLine("wynik={0} x={1} y={2} z={3}", wynik, x, y, z);
+            Console.ReadKey();
+        }
+    }
+}
