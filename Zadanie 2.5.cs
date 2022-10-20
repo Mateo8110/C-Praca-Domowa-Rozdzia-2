4ce01588dc0c556d34de65ac6dc59001ac0b1ac0
Mateo8110 <94134843+Mateo8110@users.noreply.github.com>
2022-10-20 13:22:19 +0200

    Add files via upload

---
+++ Zadanie 2.5.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Zadanie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 2, y = 3;
+            x *= y * 2;
+            Console.WriteLine(x);
+            Console.ReadKey();
+        }
+    }
+}
