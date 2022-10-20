57119741c2223affc80f6b7ddea0acfd07ba67ff
Mateo8110 <94134843+Mateo8110@users.noreply.github.com>
2022-10-20 13:19:16 +0200

    Add files via upload

---
+++ Zadanie 2.3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Zadanie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double waga, wzrost, BMI;
+
+            Console.WriteLine("Podaj wzrost w metrach: ");
+            wzrost = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj wage ciała w Kg: ");
+            waga = Double.Parse(Console.ReadLine());
+            BMI = (waga / (wzrost * wzrost));
+            Console.WriteLine(Math.Round(BMI, 2));
+            Console.ReadKey();
+            
+        }
+    }
+}
