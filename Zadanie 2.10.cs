270ac1a1a67356e4021a10f54045e8358098f561
Mateo8110 <94134843+Mateo8110@users.noreply.github.com>
2022-10-20 13:29:45 +0200

    Add files via upload

---
+++ Zadanie 2.10.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Zadanie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double powierzchnia = 100, osoby = 10;
+            double gestoscZaludnienia = osoby / powierzchnia;
+            Console.WriteLine(gestoscZaludnienia);
+            Console.ReadKey();
+        }
+    }
+}
