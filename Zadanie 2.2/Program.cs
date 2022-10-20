27baf482ccd03dffa082eac359a769f287291119
Mateo8110 <94134843+Mateo8110@users.noreply.github.com>
2022-10-20 13:17:29 +0200

    Add files via upload

---
+++ Zadanie 2.2/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Program
+{
+    class Zadanie
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c, delta;
+            Console.WriteLine("Podaj wartosc a: ");
+            a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj wartosc b: ");
+            b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj wartosc c: ");
+            c = double.Parse(Console.ReadLine());
+            delta = b * b - 4 * a * c;
+            Console.WriteLine("Wynik to: " + delta);
+
+
+
+        }
+    }
+}
