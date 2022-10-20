b69184762efeada05f25ec97c45986ca19d68079
Mateo8110 <94134843+Mateo8110@users.noreply.github.com>
2022-10-20 13:16:16 +0200

    Add files via upload

---
+++ Zadanie 2.1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Program
+{
+    class Zadanie
+    {
+        static void Main(string[] args)
+        {
+            double F, C;
+            Console.WriteLine("Podaj temp. w stopniach Celsjusza");
+            C = double.Parse(Console.ReadLine());
+            F = 32 + 9/5 * C;
+            Console.WriteLine(F);
+            Console.ReadKey();
+        }
+    }
+}
+
